780ed380c64acea332642a8d892742c1696ff324
zhang699 <t6820310@ntut.org.tw>
2026-03-17 15:18:16 +0800

    fix: move urls to top level with api key

--- lib/trading/binance.ts
+++ lib/trading/binance.ts
@@ -7,13 +7,17 @@ export const binance = new ccxt.binance({
   secret: process.env.BINANCE_API_SECRET,
   options: {
     defaultType: "future",
-    ...(true && {
-      urls: {
+  },
+  ...(isDemoTrading && {
+    urls: {
+      api: {
         fapiPublic: "https://demo-fapi.binance.com/fapi/v1",
         fapiPrivate: "https://demo-fapi.binance.com/fapi/v1",
         fapiPublicV2: "https://demo-fapi.binance.com/fapi/v2",
         fapiPrivateV2: "https://demo-fapi.binance.com/fapi/v2",
+        fapiPublicV3: "https://demo-fapi.binance.com/fapi/v3",
+        fapiPrivateV3: "https://demo-fapi.binance.com/fapi/v3",
       },
-    }),
-  },
+    },
+  }),
 });
